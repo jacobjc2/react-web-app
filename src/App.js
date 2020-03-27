1bf85d28486ceaae12c7a1ac0984ca5e3a359f43
Christensen <christensenjj@ad.msoe.edu>
2020-03-27 18:31:26 -0500

    Completed first react tutorial using SASS and Gulp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Header from './Components/headerComponent/header'
+import Footer from './Components/footerComponent/footer'
+import HomePage from './Components/pages/homePage'
+
+
+// Includes
+import './Assets/css/default.min.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+
+        <Header />
+        
+          <HomePage />
+
+        <Footer />
+
+      </div>
+    );
+  }
 }
 
 export default App;
