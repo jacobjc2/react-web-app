e7b6399034a4d762d3a11c269f56d00c6687119e
Christensen <christensenjj@ad.msoe.edu>
2020-04-01 23:13:11 -0500

    Added projects page, got tabs working, started work on the cards

--- src/Components/pages/homePage.js
+++ src/Components/pages/homePage.js
@@ -5,36 +5,39 @@ class HomePage extends Component {
   render () {
     return (
       <div className="container-fluid">
-        <div className="row">
-          <div className="banner-img">
-              <img src="https://csssaints.com/images/logos/msoe.png" alt="msoe-raiders"/>
+        <div className="home-container">
+          <div className="row">
+            <div className="banner-img">
+                <img src="https://csssaints.com/images/logos/msoe.png" alt="msoe-raiders"/>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="banner-text">
-            <h1>
-              Software Engineer
-            </h1>
-            <hr/>
-            <p>
-              HTML | C/C++ | Python | React | SQL | Agile 
-            </p>
-            <div className="social-links">
-              {/* Github Icon  */}
-              <a href="https://github.com/jacobjc2" rel="noopener noreferrer" target="_blank">
-                <i class="fa fa-github-square" aria-hidden="true"></i>
-              </a>
-              {/* LinkedIn Icon  */}
-              <a href="https://www.linkedin.com/in/jacob-christensen-23a8b5134/" rel="noopener noreferrer" target="_blank">
-                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-              </a>
-              {/* Facebook Icon  */}
-              <a href="https://www.facebook.com/jacob.christensen.501?ref=bookmarks" rel="noopener noreferrer" target="_blank">
-                <i class="fa fa-facebook-square" aria-hidden="true"></i>
-              </a>
+          <div className="row">
+            <div className="banner-text">
+              <h1>
+                Software Engineer
+              </h1>
+              <hr/>
+              <p>
+                HTML | C/C++ | Python | React | SQL | Agile 
+              </p>
+              <div className="social-links">
+                {/* Github Icon  */}
+                <a href="https://github.com/jacobjc2" rel="noopener noreferrer" target="_blank">
+                  <i class="fa fa-github-square" aria-hidden="true"></i>
+                </a>
+                {/* LinkedIn Icon  */}
+                <a href="https://www.linkedin.com/in/jacob-christensen-23a8b5134/" rel="noopener noreferrer" target="_blank">
+                  <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                </a>
+                {/* Facebook Icon  */}
+                <a href="https://www.facebook.com/jacob.christensen.501?ref=bookmarks" rel="noopener noreferrer" target="_blank">
+                  <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        
       </div>
     );
   }
--- src/Components/pages/projectPage.js
+++ src/Components/pages/projectPage.js
@@ -1,17 +1,76 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Card, Button, CardContent, CardMedia, IconButton, CardHeader, Paper } from '@material-ui/core';
 
 
 class ProjectPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0
+    };
+  }
+
+  toggleCategories() {
+    if(this.state.activeTab === 0) {
+      return (
+        <Paper elevation="2" style={{maxWidth: '345', margin: 'auto'}}>
+          <Card >
+          <CardHeader
+            
+            title="React Portfolio"
+            />
+            <CardMedia square
+            imageUrl='https://img.stackshare.io/service/1020/OYIaJ1KK.png'/>
+            <CardContent>
+                This is the code for the website you are currently viewing
+            </CardContent>
+          </Card>
+        </Paper>
+        
+      )
+    }
+    else if(this.state.activeTab === 1) {
+      return (
+        <div><h1>Python</h1></div>
+      )
+    }
+    else if(this.state.activeTab === 2) {
+      return (
+        <div><h1>C/C++</h1></div>
+      )
+    }
+    else if(this.state.activeTab === 3) {
+      return (
+        <div><h1>Other</h1></div>
+      )
+    }
+  }
+
   render () {
     return (
       <div className="container-fluid">
+        <div className="project-container">
+          <div className="project-tabs">
+            <Tabs value={this.state.activeTab} onChange={(event, tabId) => this.setState({ activeTab: tabId })} centered>
+              <Tab label="React"/>
+              <Tab label="Python"/>
+              <Tab label="C/C++"/>
+              <Tab label="Other"/>
+            </Tabs>
+          </div>
+          <section className="projects-grid">
+            <Grid container >
+              <Grid item>
+                <div className="content">
+                  {this.toggleCategories()} 
+                </div>
+              </Grid>
+              
+            </Grid>
+            
+          </section>
 
-        <h1>
-            Projects
-        </h1>
-        <p>
-            This contains information about my work.
-        </p>
+        </div>
       </div>
     );
   }
